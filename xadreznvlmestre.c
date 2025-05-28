d220c8c9d319c65667a1ce755c5eda6200947358
CiiceroMenezes <cicero.lopes.menezes@gmail.com>
2025-05-28 21:55:52 +0000

    Finalizado todos os níveis

--- xadreznvlmestre.c
+++ xadreznvlmestre.c
@@ -6,32 +6,32 @@
 // Rainha oito casas para a esquerda.
 
 #include <stdio.h>
-void recursiveTorre(int n)
+void recursiveTorre(int n) // Usando VOID para não precisar usar estruturas de loop
 {
     if (n <= 5)
     {
-        printf("Direita\n");
+        printf("Direita\n"); // Definindo o que vai ser impresso
         printf("\n");
         recursiveTorre(n + 1);
     }
 }
 
-void recursiveRainha(int n)
+void recursiveRainha(int n) // Usando VOID para não precisar usar estruturas de loop
 {
     if (n <= 8)
     {
-        printf("Esquerda\n");
+        printf("Esquerda\n"); // Definindo o que vai ser impresso
         printf("\n");
         recursiveRainha(n + 1);
     }
 }
 
-void imprimirMovimentoBispoVertical()
+void imprimirMovimentoBispoVertical() // Utilizando VOID para imprimir a mensagem desejada
 {
     printf("Cima\n");
 }
 
-void imprimirMovimentoBispoHorizontal()
+void imprimirMovimentoBispoHorizontal() // Utilizando VOID para imprimir a mensagem desejada
 {
     printf("Direita\n");
 }
@@ -40,19 +40,19 @@ int main()
     int torre = 1;
     printf("O movimento da Torre é:\n");
     printf("\n");
-    recursiveTorre(torre);
+    recursiveTorre(torre); // Acessando o calculo do VOID definido anteriormente
 
     int rainha = 1;
     printf("O movimento da Rainha é:\n");
     printf("\n");
-    recursiveRainha(rainha);
+    recursiveRainha(rainha); // Acessando o calculo do VOID definido anteriormente
 
     int bispoHorizontal = 1;
     int bispoVertical = 1;
     printf("O Movimento do Bispo é:\n");
     for (bispoVertical = 1; bispoVertical <= 5; bispoVertical++)
     {
-        for (bispoHorizontal = 1; bispoHorizontal <= 5; bispoHorizontal++)
+        for (bispoHorizontal = 1; bispoHorizontal <= 5; bispoHorizontal++) // Loops Aninhados para realizar as duas operaçoes e fazer o movimento diagonal
             ;
         {
             imprimirMovimentoBispoHorizontal();
@@ -60,14 +60,21 @@ int main()
         imprimirMovimentoBispoVertical();
         printf("\n");
     }
+    int cavaloVertical = 1;
+    int cavaloHorizontal = 1;
 
-   
-
-   
-    
-
-    
-    
+    printf("O movimento do Cavalo é:\n");
+    for (cavaloHorizontal = 1; cavaloHorizontal < 3; cavaloHorizontal++) // Definindo a direção: DIREITA
+    {
+        do
+        {
+            printf("Cima\n"); // Loop Aninhado para definir a função que se repete 2 vezes (Direção CIMA)
+            printf("\n");
+            cavaloVertical++;
+        } while (cavaloVertical < 2);
+    }
+    printf("Direita\n");
+    printf("\n");
 
     return 0;
 }
